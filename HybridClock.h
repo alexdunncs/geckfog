579164429628761aec2ccbcf897fb0b6bfe7053f
Alex Dunn <alexdunnmobile@gmail.com>
2020-10-03 18:14:48 -0700

    Integrated HybridClock into ControlStrategyScheduler. Resolves #2

--- HybridClock.h
+++ HybridClock.h
@@ -8,8 +8,7 @@
 // A clock which periodically syncs with internet time and uses millis() for most accesses
 
 class HybridClock {
-//  const unsigned long int syncThreshold = 1000*60; //Sync every minute
-  const unsigned long int syncThreshold = 1000*10; //For dev/testing
+  const unsigned long int syncThreshold = 1000*60*15; //Sync every 15 minutes
   unsigned long int lastSyncAt = 0;
   SimpleTime lastSyncTime;
   bool initialised = false;
